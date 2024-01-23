28cbcc292822df9a688b761a07b74489b194ea14
mateoghidini1998 <mateghidini123@gmail.com>
2024-01-23 16:11:35 -0300

    Create generic class ApiFilter, CustomersFilter and InvoicesFilter

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class InvoicesFilter extends ApiFilter {
+    protected $safeParms = [
+        'customerId' => ['eq'],
+        'amount' => ['eq', 'lt', 'gt', 'gte', 'lte'],
+        'status' => ['eq', 'ne'],
+        'billedDate' => ['eq', 'lt', 'gt', 'gte', 'lte'],
+        'paidDate' => ['eq', 'lt', 'gt', 'gte', 'lte'],
+        
+    ];
+
+    protected $columnMap = [
+        'customerId' => 'customer_id',
+        'billedDate' => 'billed_date',
+        'paidDate' => 'paid_date',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+        'ne' => '!=',
+    ];
+
+
+}
